316b752d0bc3c9de9225acc0d041b5e7d397ac6b
Jamie Lee <jamie@lemonbase.com>
2026-04-05 07:45:26 +0900

    Add diffview and tiny-inline-diagnostic
    - diffview: <leader>gd for git diff, <leader>gF for file history
    - tiny-inline-diagnostic: multiline inline diagnostics

---
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,8 @@
+return {
+  "sindrets/diffview.nvim",
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+    { "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+    { "<leader>gF", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview File History" },
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -0,0 +1,16 @@
+return {
+  "rachartier/tiny-inline-diagnostic.nvim",
+  event = "VeryLazy",
+  priority = 1000,
+  config = function()
+    require("tiny-inline-diagnostic").setup({
+      options = {
+        multilines = {
+          enabled = true,
+          always_show = true,
+        },
+      },
+    })
+    vim.diagnostic.config({ virtual_text = false })
+  end,
+}
