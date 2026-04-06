f1af9f31992692c796cb85a46142de17b7bd91c4
Jamie Lee <jamie@lemonbase.com>
2026-04-06 10:56:57 +0900

    Add snacks.nvim: delta diff preview, smart file picker, grep
    - Picker uses delta for diff/git preview
    - <leader>ff smart file search (hidden + ignored)
    - <leader>sg grep, <leader>ba delete all buffers
    - Explorer: <c-y> to yank filename

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,82 @@
+local snacks = require("snacks")
+
+return {
+  "snacks.nvim",
+  opts = {
+    picker = {
+      hidden = true,
+      win = {
+        input = {
+          keys = {
+            ["<a-r>"] = { "toggle_regex", mode = { "i", "n" } },
+          },
+        },
+      },
+      sources = {
+        explorer = {
+          win = {
+            list = {
+              keys = {
+                ["<c-y>"] = "explorer_yank_filename",
+              },
+            },
+          },
+        },
+      },
+      previewers = {
+        diff = {
+          builtin = false,
+          cmd = { "delta" },
+        },
+        git = {
+          builtin = false,
+          args = { "-c", "core.pager=delta" },
+        },
+      },
+      actions = {
+        explorer_yank_filename = function(picker)
+          local items = picker:selected({ fallback = true })
+          if #items > 0 then
+            local filenames = {}
+            for _, item in ipairs(items) do
+              table.insert(filenames, vim.fn.fnamemodify(item.file, ":t"))
+            end
+            local value = table.concat(filenames, "\n")
+            vim.fn.setreg("+", value)
+            snacks.notify.info("Copied " .. #filenames .. " filename(s)")
+          end
+        end,
+      },
+    },
+  },
+
+  keys = {
+    {
+      "<leader>ff",
+      function()
+        snacks.picker.smart({
+          hidden = true,
+          ignored = true,
+        })
+      end,
+      desc = "Find Files",
+    },
+    {
+      "<leader>sg",
+      function()
+        snacks.picker.grep({
+          hidden = true,
+          regex = false,
+        })
+      end,
+      desc = "Grep",
+    },
+    {
+      "<leader>ba",
+      function()
+        snacks.bufdelete.all()
+      end,
+      desc = "Delete all buffers",
+    },
+  },
+}
