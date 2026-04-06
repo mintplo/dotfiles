99f26c399d011cc745e4b7993d8e524829f23ac1
Jamie Lee <jamie@lemonbase.com>
2026-04-06 10:28:33 +0900

    Add keymaps: blackhole delete, close buffer, disable macro
    - d → blackhole register (preserve clipboard on delete)
    - <C-b> → close current buffer
    - q → disabled (prevent accidental macro recording)

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
+-- Delete without overwriting clipboard
+vim.keymap.set("n", "d", '"_d')
+vim.keymap.set("v", "d", '"_d')
+
+-- Close buffer
+vim.keymap.set("n", "<C-b>", "<cmd>:bd<CR>", { desc = "Close buffer" })
+
+-- Disable macro recording
+vim.keymap.set("n", "q", "")
